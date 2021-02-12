8968ee56d99da1bdc42041ae1fda6c2504f1323e
Diego Sierraalta <dsierraalta@lcs.on.ca>
2021-02-12 11:44:11 -0500

    Example of suing a Picker View

--- iOS-DataStorageConverter/ContentView.swift
+++ iOS-DataStorageConverter/ContentView.swift
@@ -11,15 +11,24 @@ struct ContentView: View {
     
     @State private var name: String = ""
     
+    @State private var feeling: String = ""
+    
     private var greeting: String {
-        return "Hello, \(name)"
+        return "Hello, \(name), you are \(feeling)"
     }
     
     var body: some View {
         
-        VStack {
+        Form {
             
             TextField("Enter your name", text: $name)
+            
+            Picker("Mood", selection: $feeling) {
+                Text("😄").tag("happy")
+                Text("🙂").tag("fine")
+                Text("😢").tag("sad")
+            }
+            .pickerStyle(SegmentedPickerStyle())
             
             Text(greeting)
                 .padding()
