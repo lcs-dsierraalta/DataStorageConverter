0ef739edc9a24e0365fbc75b895d660795fa7d89
Diego Sierraalta <dsierraalta@lcs.on.ca>
2021-02-12 11:10:37 -0500

    Added iOS target using SwiftUI framework to project

---
+++ iOS-DataStorageConverter/iOS_DataStorageConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_DataStorageConverterApp.swift
+//  iOS-DataStorageConverter
+//
+//  Created by Diego Sierraalta on 2021-02-12.
+//
+
+import SwiftUI
+
+@main
+struct iOS_DataStorageConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
